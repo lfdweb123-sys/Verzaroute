e1b2d4e9ed73fb8a6a278f7216c629099d5e4146
Gerard Sononkpon <lfdweb123@gmail.com>
2026-07-15 22:44:10 +0100

    first commit

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,12 +4,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
   return (
     <div className="min-h-screen bg-obsidian">
       <DashboardSidebar />
-      
-      {/* Marge gauche dynamique qui suit l'état collapsed de la sidebar */}
-      {/* Note: Si vous ne pouvez pas accéder à l'état isCollapsed ici, 
-          utilisez une classe CSS conditionnelle ou déplacez ce layout 
-          dans un composant client qui partage l'état */}
-      <main className="md:ml-64 pt-16 pb-20 md:pt-0 md:pb-0 px-4 sm:px-6 lg:px-8 transition-all duration-300">
+      <main className="md:ml-64 pt-24 pb-20 md:pt-24 md:pb-8 px-4 sm:px-6 lg:px-8 transition-all duration-300">
         {children}
       </main>
     </div>
